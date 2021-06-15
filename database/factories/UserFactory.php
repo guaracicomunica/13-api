e2106d1be2fb8cef0bffa5764b40d76a969b325b
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-06-15 00:02:01 -0300

    delete template migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -13,7 +12,7 @@ class UserFactory extends Factory
      *
      * @var string
      */
-    protected $model = User::class;
+    /*protected $model = User::class;*/
 
     /**
      * Define the model's default state.
@@ -23,11 +22,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            /*'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(10),*/
         ];
     }
 
@@ -36,12 +35,12 @@ class UserFactory extends Factory
      *
      * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public function unverified()
+    /*public function unverified()
     {
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
             ];
         });
-    }
+    }*/
 }
