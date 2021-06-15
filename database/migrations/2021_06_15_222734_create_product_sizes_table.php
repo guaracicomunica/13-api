928c9c2c964ad560ce3c4c0d2f6f1f0065db8abe
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-06-15 00:02:51 -0300

    create migrations

---
+++ database/migrations/2021_06_15_222734_create_product_sizes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductSizesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_sizes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('size_id');
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('size_id')->references('id')->on('sizes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_sizes');
+    }
+}
