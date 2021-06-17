e30540efe43eb97fdfb7aa5990a8c41fb576a7ad
João Paulo <jpaulope@indracompany.com>
2021-06-16 21:39:48 -0300

    feat: relacionamento entre pedido, status, usuário e carrinho de compras

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -15,4 +15,19 @@ class Cart extends Model
         'status_id',
         'amount',
     ];
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function status()
+    {
+        return $this->hasOne(OrderStatus::class);
+    }
 }
