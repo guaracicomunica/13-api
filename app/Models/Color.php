5e087782543401ed3efbfa9236594866d52796d1
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-08-11 22:15:39 -0300

    feat: create colors table

---
+++ app/Models/Color.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Color extends Model
+{
+    use HasFactory;
+
+    protected $table = 'colors';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'hex_code'
+    ];
+
+}
