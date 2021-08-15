58e1eb60af2473375aba7ab8c9b91c18463ac87a
Diogo Lima <dsilval@indracompany.com>
2021-08-15 20:19:49 -0300

    feat: tradução das validações no laravel.

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'pt-br',
 
     /*
     |--------------------------------------------------------------------------
@@ -89,7 +89,7 @@ return [
     |
     | The fallback locale determines the locale to use when the current one
     | is not available. You may change the value to correspond to any of
-    | the language folders that are provided through your application.
+    | the language folders that are provided through your appl  ication.
     |
     */
 
