b62bc00b64cfbcb467e55b14dc10413b7a183c4d
João Paulo <jpaulope@indracompany.com>
2021-11-09 17:48:05 -0300

    feat: make hash of default user password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
